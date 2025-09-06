6fc66686dce06f42a014ec2c5f689b69b32c748f
tewereus <tewereus12312@gmail.com>
2025-09-06 12:07:55 +0300

    feat(notification)

--- app/app/_layout.js
+++ app/app/_layout.js
@@ -7,47 +7,72 @@ import i18n from "../locals/i18n";
 import ToastProvider from "react-native-toast-message";
 import React, { memo } from "react";
 import "./index.css";
+import { NotificationProvider } from "@/context/NotificationContext";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
 // make it so that the routes are accessable for authenticated users, if user logged out and clicked back it goes back to the page
 const _layout = memo(() => {
   return (
     <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="seller_tabs" options={{ headerShown: false }} />
-          <Stack.Screen name="seller_auth" options={{ headerShown: false }} />
-          <Stack.Screen name="notification" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="create_property"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="profile_management"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="payment_post" options={{ headerShown: false }} />
-          <Stack.Screen name="boost_payment" options={{ headerShown: false }} />
-          <Stack.Screen name="edit_property" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="payment_success"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="payment-webview"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="transaction_history"
-            options={{
-              headerShown: false,
-            }}
-          />
-          {/* <Stack.Screen name="map_component" options={{ headerShown: false }} /> */}
-        </Stack>
-        <ToastProvider />
-      </I18nextProvider>
+      <NotificationProvider>
+        <I18nextProvider i18n={i18n}>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="seller_tabs" options={{ headerShown: false }} />
+            <Stack.Screen name="seller_auth" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="notification"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="create_property"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="profile_management"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="payment_post"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="boost_payment"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="edit_property"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="payment_success"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="payment-webview"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="transaction_history"
+              options={{
+                headerShown: false,
+              }}
+            />
+            {/* <Stack.Screen name="map_component" options={{ headerShown: false }} /> */}
+          </Stack>
+          <ToastProvider />
+        </I18nextProvider>
+      </NotificationProvider>
     </Provider>
   );
 });
--- app/app/index.js
+++ app/app/index.js
@@ -43,3 +43,54 @@ const Index = () => {
 };
 
 export default Index;
+
+// import { useEffect, useState } from "react";
+// import {
+//   Alert,
+//   Button,
+//   Platform,
+//   SafeAreaView,
+//   StatusBar,
+//   Text,
+//   View,
+// } from "react-native";
+// import { useNotification } from "@/context/NotificationContext";
+// // import * as Updates from "expo-updates";
+
+// export default function HomeScreen() {
+//   const { notification, expoPushToken, error } = useNotification();
+
+//   if (error) {
+//     return <Text>Error: {error.message}</Text>;
+//   }
+
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         padding: 50,
+//         marginTop: 30,
+//         paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 10,
+//       }}
+//     >
+//       <SafeAreaView style={{ flex: 1 }}>
+//         <Text type="subtitle">Updates Demo 5</Text>
+//         <Text
+//           type="subtitle"
+//           style={{ color: "red" }}
+//           onClick={() => console.log("notification")}
+//         >
+//           Your push token:
+//         </Text>
+//         <Text>{expoPushToken}</Text>
+//         <Text type="subtitle" onClick={() => console.log("notification")}>
+//           Latest notification:
+//         </Text>
+//         <Text>{notification?.request.content.title}</Text>
+//         <Text>
+//           {JSON.stringify(notification?.request.content.body, null, 2)}
+//         </Text>
+//       </SafeAreaView>
+//     </View>
+//   );
+// }
